a035e0797c44045d6400b995319f353fc04b36c5
Tibrella <pinghigh24678@outlook.com>
2023-09-09 16:02:56 +0800

    fix: 采用更好的指定配置文件的方式

--- entrypoint.sh
+++ entrypoint.sh
@@ -34,10 +34,10 @@ chown -R builder .
 pacman -S --noconfirm --needed paru
 
 if test -z "${INPUT_MAKEPKGPROFILEPATH}";then
-	echo "Didn't provide makepkg profile path. Skipped."
+	sudo -H -u builder paru -S --noconfirm --clonedir . $pkgname
 else
-	sudo -H -u builder install -D /etc/makepkg.conf ~/.config/pacman/makepkg.conf # 怕出权限问题所以先传统方式安装
-	sudo -H -u cat ${INPUT_MAKEPKGPROFILEPATH} > ~/.config/pacman/makepkg.conf
+    chmod -R a+rw ${INPUT_MAKEPKGPROFILEPATH}
+	sudo -H -u builder paru -S --noconfirm --clonedir . $pkgname -mflags \"--config ${INPUT_MAKEPKGPROFILEPATH}\"
 fi
 
 sudo -H -u builder paru -U --noconfirm --mflags "${INPUT_MAKEPKGARGS:-}"
